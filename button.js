c86cb82a76cfa5ff1276a032221ac370c413d591
hai99 <thaiviethai99@gmail.com>
2019-02-25 13:45:27 +0700

    fisrt code

---
+++ button.js
@@ -0,0 +1,20 @@
+ <TouchableOpacity onPress={this.Send_Data_Function} activeOpacity={0.7} style={styles.button} >
+
+          <Text style={styles.buttonText}> Send TextInput Value To Next Activity </Text>
+
+        </TouchableOpacity>
+
+    button: {
+
+    width: '90%',
+    height: 40,
+    padding: 10,
+    backgroundColor: '#4CAF50',
+    borderRadius: 8,
+    marginTop: 15
+  },
+
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
